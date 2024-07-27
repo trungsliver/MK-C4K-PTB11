bb8a7f1cd3c24eb097606c83fadd5ef0eb4bbe6c
trungsliver <buiductrung.cva@gmail.com>
2024-07-27 10:23:46 +0700

    hackathon

---
+++ hackathon.py
@@ -0,0 +1,15 @@
+# Quy tắc đặt tên: [Tên lớp]_[Họ tên]_hackathon.py
+# VD: PTB11_DucTrung_hackathon.py
+
+# Đánh dấu bài
+    # Tuấn Minh:
+    # Chí Bảo:
+    # Bảo Phúc:
+    # Minh Tú: 1
+    # Đức Tùng:
+    # Khang Anh:  -1 -1 -0.5 -0.5
+
+# Ví dụ làm tròn
+a = 8.8888888
+a = round(a, 2)
+print(a)
